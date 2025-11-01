d7c44236840586849937d11fcb5ff5f6fd89a71b
SenlaIvan <you@example.com>
2025-11-01 20:31:25 +0300

    add cypress

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 1000,
+    host: 'localhost',
+    strictPort: true, 
+    open: true,
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
