7d919a04fb59ab4bf49d52388443c48ab69de974
SenlaIvan <you@example.com>
2025-11-01 21:12:17 +0300

    integration test

--- cypress/e2e/simpleIntegrationsTests.cy.js
+++ cypress/e2e/simpleIntegrationsTests.cy.js
@@ -1,5 +1,24 @@
-describe('integrations tetst', () => {
-    it('login', () => {
-        cy.visit('/');
+Cypress.Commands.add('login', (email, passowrd) => {
+    cy.visit('/');
+
+    cy.get('#login_email').type(email);
+    cy.get('#login_passowrd').type(passowrd);
+
+    cy.contains('button', 'Войти').click();
+});
+
+describe('интеграционные тесты', () => {
+    beforeEach(() => {
+        cy.login('login', 'psw');
+    });
+
+    it('авторизация', () => {
+        cy.url().should('contain', '/home');
+    })
+
+    it('вкладка профиль', () => {
+        cy.xpath("//button[span[contains(text(), 'Профиль')]]").click();
+        cy.url().should('contain', '/userProfile');
+        cy.get('h3').should('have.text', 'Cтраница профиля сотрудника');
     })
 })
